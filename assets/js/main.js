6a23fcb8e0af0ddb4b6e3ebff47cb98bc2ad5b5f
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-03-01 01:45:14 +0100

    fix non click logic, fix click counter, edit js comment and html-css style

--- assets/js/main.js
+++ assets/js/main.js
@@ -193,8 +193,11 @@ function flowerPositioning(grid, positions) {
     for (let i = 0; i < grid.length; i++) {
 
         if (positions.includes(i)) {
-            // grid[i].innerHTML = 'F';
+            // grid[i].innerHTML = 'F'; (placeholder)
             grid[i].classList.add('flower')
+            grid[i].innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-flower3" viewBox="0 0 16 16">
+            <path d="M11.424 8c.437-.052.811-.136 1.04-.268a2 2 0 0 0-2-3.464c-.229.132-.489.414-.752.767C9.886 4.63 10 4.264 10 4a2 2 0 1 0-4 0c0 .264.114.63.288 1.035-.263-.353-.523-.635-.752-.767a2 2 0 0 0-2 3.464c.229.132.603.216 1.04.268-.437.052-.811.136-1.04.268a2 2 0 1 0 2 3.464c.229-.132.489-.414.752-.767C6.114 11.37 6 11.736 6 12a2 2 0 1 0 4 0c0-.264-.114-.63-.288-1.035.263.353.523.635.752.767a2 2 0 1 0 2-3.464c-.229-.132-.603-.216-1.04-.268M9 4a2 2 0 0 1-.045.205q-.059.2-.183.484a13 13 0 0 1-.637 1.223L8 6.142l-.135-.23a13 13 0 0 1-.637-1.223 4 4 0 0 1-.183-.484A2 2 0 0 1 7 4a1 1 0 1 1 2 0M3.67 5.5a1 1 0 0 1 1.366-.366 2 2 0 0 1 .156.142q.142.15.326.4c.245.333.502.747.742 1.163l.13.232-.265.002a13 13 0 0 1-1.379-.06 4 4 0 0 1-.51-.083 2 2 0 0 1-.2-.064A1 1 0 0 1 3.67 5.5m1.366 5.366a1 1 0 0 1-1-1.732l.047-.02q.055-.02.153-.044.202-.048.51-.083a13 13 0 0 1 1.379-.06q.135 0 .266.002l-.131.232c-.24.416-.497.83-.742 1.163a4 4 0 0 1-.327.4 2 2 0 0 1-.155.142M9 12a1 1 0 0 1-2 0 2 2 0 0 1 .045-.206q.058-.198.183-.483c.166-.378.396-.808.637-1.223L8 9.858l.135.23c.241.415.47.845.637 1.223q.124.285.183.484A1.3 1.3 0 0 1 9 12m3.33-6.5a1 1 0 0 1-.366 1.366 2 2 0 0 1-.2.064q-.202.048-.51.083c-.412.045-.898.061-1.379.06q-.135 0-.266-.002l.131-.232c.24-.416.497-.83.742-1.163a4 4 0 0 1 .327-.4q.07-.074.114-.11l.041-.032a1 1 0 0 1 1.366.366m-1.366 5.366a2 2 0 0 1-.155-.141 4 4 0 0 1-.327-.4A13 13 0 0 1 9.74 9.16l-.13-.232.265-.002c.48-.001.967.015 1.379.06q.308.035.51.083.098.024.153.044l.048.02a1 1 0 1 1-1 1.732zM8 9a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
+          </svg>`;
         }
     };
 };
@@ -289,7 +292,7 @@ if (clicksNumber === winNumber) {
 form.addEventListener('submit', function (e) {
     e.preventDefault()
 
-
+//----generate the grid
     const difficulty = document.querySelector('select').value;
     // console.log(difficulty);
 
@@ -303,18 +306,18 @@ form.addEventListener('submit', function (e) {
     };
     // console.log(gridSideLength);
 
-    console.clear();
-
     const cells = createGrid(gridSideLength);
     // console.log(cells);
 
+//----add flowers
     const flowerPositionNumbers = getFlowersPosition(gridSideLength);
     // console.log(flowerPositionNumbers);
 
     flowerPositioning(cells, flowerPositionNumbers);
 
-    // const winNumber = cells.length - flowerPositionNumbers.length;
-    const winNumber = 5;
+//----play the game
+    const winNumber = cells.length - flowerPositionNumbers.length;
+    // const winNumber = 5; (debug)
 
     let clicksNumber = 0;
 
@@ -324,106 +327,204 @@ form.addEventListener('submit', function (e) {
         cells[i].addEventListener('click', click);
     };
 
+//----end the game
+    /**
+     * add click function and check the cell
+     * click add class active and increment the click counter
+     * check when the game has to finish
+     * if click flower or click all the other cells
+     * then block the game removing the eventListener(click)
+     * give the type of end through the flower variable to print the result
+     */
     function click() {
 
         this.classList.add('active');
-
+        this.removeEventListener('click', click);
         clicksNumber++
 
         if (this.classList.contains('flower')) {
+            document.querySelectorAll('.flower').forEach((flower) => flower.classList.add('active'));
             for (let i = 0; i < cells.length; i++) {
                 cells[i].removeEventListener('click', click);
             };
-            console.log('yes');
+            // console.log('yes');
             flower = 'found';
 
         } else if (clicksNumber === winNumber) {
+            this.classList.add('active');
             for (let i = 0; i < cells.length; i++) {
                 cells[i].removeEventListener('click', click);
             };
-            console.log('not');
-            flower ='not found';
+            // console.log('not');
+            flower = 'not found';
 
-        } else {
-            document.querySelector('.cell').removeEventListener('click', click);
-        };
-        console.log(clicksNumber);
-        console.log(flower);
-        
+        }
+
+        // console.log(clicksNumber);
+        // console.log(flower);
+//----print the result
         const attempt = clicksNumber === 1 ? 'try' : 'tries';
         const foundMessage = `You found the FLOWER in ${clicksNumber} ${attempt}`;
         const notFoundMessage = `You missed the FLOWER all the tries ( ${clicksNumber} )`;
-    
+
         if (flower === 'found') {
-            console.log('FOUND');
+            // console.log('FOUND');
             createResultMessage(foundMessage);
         } else if (flower === 'not found') {
-            console.log('NOT FOUND');
+            // console.log('NOT FOUND');
             createResultMessage(notFoundMessage);
         };
     };
-
-    
-
-
-    /* cells.forEach((cell) => cell.addEventListener('click', function click() {
-
-        clicksNumber++
-
-        if (cell.classList.contains('flower')) {
-            activateCell(cell);
-            // console.log('yes');
-            flower = 'found'
-            
-            cells.forEach((cell) => cell.removeEventListener('click', click))
-             for (let i = 0; i < cells.length; i++) {
-                cells[i].removeEventListener("click", click);
-            }
-            } else if (clicksNumber === winNumber) {
-                activateCell(cell);
-                // console.log('not');
-                flower = 'notFound'
-            } else if (!cell.classList.contains('active')) {
-                activateCell(cell);
-                
-            };
-
-            console.log(clicksNumber);
-
-            console.log(flower);
-
-            const foundMessage = `You found the FLOWER in ${clicksNumber} try`;
-            const notFoundMessage = `You didn't find the FLOWER in ${clicksNumber} try`;
-
-            if (flower === 'found') {
-                // console.log('FOUND');
-                createResultMessage(foundMessage);
-            } else if (flower === 'notFound') {
-                // console.log('NOT FOUND');
-                createResultMessage(notFoundMessage);
-            };
-
-        
-    })) */
 });
 
 
-/* function stopClick(cells, click) {
-    // cells.forEach((cell) => cell.removeEventListener('click', click))
-    for (let i = 0; i < cells.length; i++) {
-        const element = cells[i];
-        element.removeEventListener('click', click)
-    }
-} */
 
 
 
 
-// const clickCells = document.getElementsByClassName("cell");
 
 
-/* function click() {
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].removeEventListener("click", click);
-    }
-} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
